42a92785616b14b257f6ea569f47ff761ac24973
Macquet Quentin <qunetinmacquet@gmail.com>
2023-04-11 22:25:31 +0200

    ADD: sites

---
+++ emissions.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Blind Test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="Blind Test.png">
+        <h1>Blind Test</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="films.html">Musiques de Films</a></li>
+            <li><a href="emissions.html">Musiques d'émissions</a></li>
+            <li><a href="70.html">Musiques des années 70</a></li>
+            <li><a href="80.html">Musiques des années 80</a></li>
+            <li><a href="90.html">Musiques des années 90</a></li>
+            <li><a href="2000.html">Musiques des années 2000</a></li>
+        </ul>
+    </nav>
+    <div>
+        <h2>Musiques d'émissions</h2>
+        <p>Question : Quel est le titre de cette chanson ?</p>
+        <form>
+            <input type="text" name="answer" placeholder="Votre réponse">
+            <button type="submit">Valider</button>
+        </form>
+    </div>
+    <footer>
+        <p>© 2023 Blind Test. Tous droits réservés sinon le procès assuré.</p>
+    </footer>
+</body>
+
+</html>
