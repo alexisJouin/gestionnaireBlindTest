506850fe8c8b73a7e932ccbc8d23656c5cf212a4
MathieuRomain <126685006+MathieuRomain@users.noreply.github.com>
2023-05-02 19:23:04 +0200

    Emissions works now :)

--- emissions.php
+++ emissions.php
@@ -20,10 +20,105 @@
     </nav>
     <div>
         <h2>Musiques d'émissions</h2>
-        <p>Question : Quel est le titre de cette chanson ?</p>
-        <form>
+        <form method= "post">
+            <p>Question : Quel est le titre de cette chanson ?</p>
+                       
+
+            <?php
+        
+        session_start();
+        $dossier = 'music';
+        
+        // Tableau contenant les noms des dossiers à explorer
+        $dossiers = array('emissions');
+        
+        // Tableau pour stocker les fichiers trouvés
+        $fichiers = array();
+
+        // Boucle sur les dossiers pour récupérer les fichiers
+        foreach ($dossiers as $dossier_nom) {
+            $dossier_complet = $dossier . '/' . $dossier_nom;
+            // Si le dossier existe
+            if (file_exists($dossier_complet)) {
+                // Récupération de la liste des fichiers dans le dossier
+                $fichiers_dossier = glob($dossier_complet . '/*.*');
+                // Ajout des fichiers trouvés au tableau
+                $fichiers = array_merge($fichiers, $fichiers_dossier);
+            }
+        }
+        //Fonction qui découpe le nom du fichier pour séparer les info
+        function découpe($fichier_aleatoire){
+            $découpe = explode('_', $fichier_aleatoire);
+            $_SESSION['annee'] = $découpe[1];
+            $nomExtension = $découpe[2];
+            $découpeNomExtension = explode('.',$nomExtension);
+            $_SESSION['nom'] = $découpeNomExtension[0]; 
+            $_SESSION['nom'] = str_replace("-"," ",$_SESSION['nom']);
+            //Affichage des info
+            echo "<p>nom de l'émission: ".$_SESSION['nom']."<br>"; 
+            echo "année: ".$_SESSION['annee']."</p>";
+        }
+
+        function aléatoire($fichiers){
+            global $point;
+            // Choix aléatoire d'un fichier dans le tableau
+            $fichier_aleatoire = $fichiers[array_rand($fichiers)];
+            
+            // Affichage du nom du fichier choisi
+            
+            echo "<audio controls autoplay>";
+            echo "<source src='",$fichier_aleatoire,"' type='audio/mpeg'>";
+            echo "</audio>";
+            découpe($fichier_aleatoire);
+            echo "<p>le fichier de la musique est ".$fichier_aleatoire."</p>";
+            echo "<p>nb de points ".$_SESSION['points']."<p>";
+            
+        }
+
+        if (!isset($_SESSION['points'])) {
+            $_SESSION['points'] = -1;
+        }
+        if (isset($_POST['boutonValider'])) {
+            if ($_POST['answer']==$_SESSION['nom']){
+                $_SESSION['points']++;
+                if($_SESSION['points']>3){
+                    echo "<h2>Vous avez gagné!</h2>";
+                    session_destroy();
+                }else{
+                    echo aléatoire($fichiers);
+                }
+            }else{
+                echo aléatoire($fichiers);
+            }
+            
+        }
+
+        if (isset($_POST["boutonReset"])){
+            session_destroy();
+            $_SESSION['points']=-1;
+        }
+        
+        //programme pour fair un tableau avec les données json
+        $json_data = file_get_contents('music/musique.json');
+        $data = json_decode($json_data, true);
+
+        // foreach ($data as $themes => $theme) {
+        //     echo $themes . ":\n";
+        //     echo "<br>";
+        //     foreach ($theme as $nom => $tana) {
+        //         echo " " . $nom . "\n";
+        //         echo"<br>";
+        //         foreach ($tana as $tana_key => $values) {
+        //             echo "     " . $tana_key . ": " . $values . "\n";
+        //             echo"<br>";
+        //         }
+        //     }
+        // }
+
+    ?>
             <input type="text" name="answer" placeholder="Votre réponse">
-            <button type="submit">Valider</button>
+            <button type="submit" name="boutonValider">Valider</button>
+            <input type="submit" name="boutonReset" value="reset">
         </form>
     </div>
     <footer>
